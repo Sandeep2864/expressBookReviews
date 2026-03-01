7318dd4bd86ff0b60464eda381d9c7d630e1eeeb
Sandeep Reddy <2200030956@kluniversity.in>
2026-03-01 04:31:15 -0500

    small change in the router of common user for getting the books route updated code -2

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -20,13 +20,36 @@ public_users.get('/',function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn=req.params.isbn;
+
+  if(books[isbn]) {
+    return res.status(300).send(JSON.stringify(books[isbn],null,4));
+  }
+  else {
+     return res.status(404).json({message:"Book not found"});
+  }
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
+    let author=req.params.author;
+    let result= [];
+
+    let keys=Object.keys(books);
+
+    keys.forEach((key) => {
+        if(books[key].author===author) {
+            result.push(books[key]);
+        }
+    })
+
+    if(result.length>0) {
+        return res.status(200).send(JSON.stringify(result,null,4));
+    }
+    else {
+        return res.status(404).json({message: "No books found for this author"});
+    }
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get all books based on title
